3bad48f69225493f78ca9b9c7b2983e2f5c8838a
Bharat <bharat.yadav@ninebit.in>
2024-03-21 17:25:35 +0530

    Intail commit

---
+++ webapp/controller/Main.controller.js
@@ -0,0 +1,33 @@
+sap.ui.define(
+	["./BaseController", "sap/ui/model/json/JSONModel"],
+	function (BaseController, JSONModel) {
+		"use strict";
+
+		return BaseController.extend("com.myorg.myapp.controller.Main", {
+			onInit: function () {
+				var oModel = new JSONModel();
+				this.getView().setModel(oModel, "entity");
+
+				this.fetchData();
+			},
+
+			fetchData: function () {
+				var sApiUrl =
+					"https://services.odata.org/TripPinRESTierService/(S(id))/People";
+
+				jQuery.ajax({
+					url: sApiUrl,
+					method: "GET",
+					dataType: "json",
+					success: function (oData) {
+						console.log(oData);
+						this.getView().getModel("entity").setData(oData);
+					}.bind(this),
+					error: function (error) {
+						console.error("Error fetching API data:", error);
+					},
+				});
+			},
+		});
+	}
+);
